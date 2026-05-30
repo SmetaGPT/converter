04b0d2b6c4cb8eebefecac2dba2dbdcf3ae395dd
Sergei G <kazpost@gmail.com>
2026-05-30 19:51:52 +0300

    Finalize inline glyph pyright fix

--- src/doc_converter/converters/docx/inline_glyph.py
+++ src/doc_converter/converters/docx/inline_glyph.py
@@ -324,5 +324,4 @@ def _render_inline_glyph_template(symbol: str, font_name: str, font_size: int) -
 
 def _inline_glyph_difference_score(image: Image.Image, template: Image.Image) -> float:
     diff = ImageChops.difference(image, template)
-    pixel_values = list(diff.getdata())
-    return sum(pixel_values) / (255 * INLINE_GLYPH_TEMPLATE_SIZE * INLINE_GLYPH_TEMPLATE_SIZE)
+    return sum(diff.tobytes()) / (255 * INLINE_GLYPH_TEMPLATE_SIZE * INLINE_GLYPH_TEMPLATE_SIZE)
